75b9d72764b145a20b01eba7d359263124881075
idk <idk@users.noreply.github.com>
2026-09-15 21:49:23 +0700

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import react from '@vitejs/plugin-react'
+  import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import path from 'path'
