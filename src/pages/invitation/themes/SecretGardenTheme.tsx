277e17572e8875156c77e2097615c5df24ec2152
idk <idk@users.noreply.github.com>
2026-09-25 11:11:09 +0700

    style: exactly match Utensils and Praying hands icons with design

--- src/pages/invitation/themes/SecretGardenTheme.tsx
+++ src/pages/invitation/themes/SecretGardenTheme.tsx
@@ -1449,10 +1449,14 @@ export const SecretGardenTheme: React.FC<SecretGardenThemeProps> = ({
                   {/* 2. Makan & Minum */}
                   <div className="flex flex-col items-center text-center">
                     <div className="w-[58px] h-[58px] sm:w-[62px] sm:h-[62px] rounded-full border border-white/40 flex items-center justify-center mb-2 bg-white/10">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" className="w-7 h-7 sm:w-8 sm:h-8 stroke-white">
-                        <path d="M3 2v7c0 1.1.9 2 2 2h4a2 2 0 0 0 2-2V2"/>
-                        <path d="M7 2v20"/>
-                        <path d="M21 15V2v0a5 5 0 0 0-5 5v6c0 1.1.9 2 2 2h3Zm0 0v7"/>
+                      <svg className="w-7 h-7 sm:w-8 sm:h-8 stroke-white stroke-[2.2] fill-none" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round">
+                        {/* Fork */}
+                        <path d="M8 21v-9" />
+                        <path d="M5 6v4a3 3 0 0 0 6 0V6" />
+                        <path d="M8 6v4" />
+                        {/* Knife / Spoon */}
+                        <path d="M16 21v-9" />
+                        <path d="M16 12c-2.5 0-2.5-7 0-7s2.5 7 0 7z" />
                       </svg>
                     </div>
                     <p 
@@ -1466,14 +1470,13 @@ export const SecretGardenTheme: React.FC<SecretGardenThemeProps> = ({
                   {/* 3. Mendoakan */}
                   <div className="flex flex-col items-center text-center">
                     <div className="w-[58px] h-[58px] sm:w-[62px] sm:h-[62px] rounded-full border border-white/40 flex items-center justify-center mb-2 bg-white/10">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" className="w-7 h-7 sm:w-8 sm:h-8 stroke-white">
-                        <path d="M18 11V6a2 2 0 0 0-2-2h-1.5a2 2 0 0 0-2 2v5"/>
-                        <path d="M6 11V6a2 2 0 0 1 2-2h1.5a2 2 0 0 1 2 2v5"/>
-                        <path d="M12 11v6"/>
-                        <path d="M4 11h16"/>
-                        <path d="M16 14s1.5-2 3-2 3 2 3 2v2a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3v-2s1.5-2 3-2 3 2 3 2"/>
-                        <path d="M9 14v4"/>
-                        <path d="M15 14v4"/>
+                      <svg className="w-7 h-7 sm:w-8 sm:h-8 stroke-white stroke-[2.2] fill-none" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round">
+                        {/* Center touch line */}
+                        <path d="M12 15V6a2 2 0 0 0-4 0v5" />
+                        <path d="M12 15V6a2 2 0 0 1 4 0v5" />
+                        {/* Cupped palms */}
+                        <path d="M12 15H5a2 2 0 0 1 0-4h3" />
+                        <path d="M12 15h7a2 2 0 0 0 0-4h-3" />
                       </svg>
                     </div>
                     <p 
